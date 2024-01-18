4becb10e95d7a63f395b340c7602edae59c3e7b2
Juan <juandelcirco@gmail.com>
2024-01-18 15:03:08 -0400

    chaged if to switch

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function calcularIMC() {
+const getIMC=(imc)=>{
+    switch(imc){
+        case imc < 18.5:
+            return 'Bajo peso';
+        case imc >= 18.5 && imc <= 24.9:
+            return 'Peso normal';
+        case imc >= 25 && imc <= 29.9:
+            return 'Sobrepeso';
+        default:
+            return 'Obesidad';
+    }
+}
+const calcularIMC=()=>{
     let altura = document.getElementById('altura').value;
     let peso = document.getElementById('peso').value;
 
     if (altura !== '' && peso !== '') {
         let alturaMetros = altura / 100;
         let imc = peso / (alturaMetros * alturaMetros);
-
-        let resultado = '';
-
-        if (imc < 18.5) {
-            resultado = 'Bajo peso';
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            resultado = 'Peso normal';
-        } else if (imc >= 25 && imc <= 29.9) {
-            resultado = 'Sobrepeso';
-        } else {
-            resultado = 'Obesidad';
-        }
-
+        let resultado = getIMC(imc)
         document.getElementById('result').innerText = 'Su IMC es ' + imc.toFixed(2) + '. ' + resultado;
     } else {
         alert('Por favor, complete todos los campos.');
